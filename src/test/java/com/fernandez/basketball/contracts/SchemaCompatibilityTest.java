732c6b52a66a914c21cc009b5a3013c9f293e194
Juan Francisco Fernandez Herreros <31112111+juanfranciscofernandezherreros@users.noreply.github.com>
2026-09-25 22:07:22 +0200

    KAN-84 assert Avro null default correctly

--- src/test/java/com/fernandez/basketball/contracts/SchemaCompatibilityTest.java
+++ src/test/java/com/fernandez/basketball/contracts/SchemaCompatibilityTest.java
@@ -11,7 +11,6 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class SchemaCompatibilityTest {
@@ -32,7 +31,7 @@ class SchemaCompatibilityTest {
         assertEquals(Schema.Type.UNION, expectedRows.schema().getType());
         assertTrue(expectedRows.schema().getTypes().stream().anyMatch(type -> type.getType() == Schema.Type.NULL));
         assertTrue(expectedRows.schema().getTypes().stream().anyMatch(type -> type.getType() == Schema.Type.LONG));
-        assertNull(expectedRows.defaultVal());
+        assertTrue(expectedRows.hasDefaultValue());
     }
 
     @Test
